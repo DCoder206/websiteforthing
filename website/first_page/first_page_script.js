27b38679d41807a19f45316dd93d89de6d5e8c27
DCoder206 <62135445+DCoder206@users.noreply.github.com>
2023-08-09 14:40:44 +0530

    Update first_page_script.js

--- website/first_page/first_page_script.js
+++ website/first_page/first_page_script.js
@@ -6,7 +6,7 @@ but.addEventListener("click",(event) => {
     event.preventDefault();
     const inp = document.querySelector("#schName").value.toLowerCase()
     if (inp.length !== 0 && inp.includes("school")) {
-        redirect("../main_page/index.html")
+        redirect("../main_page/main_page.html")
     }
     else {
         const div = document.querySelector("#msg");
