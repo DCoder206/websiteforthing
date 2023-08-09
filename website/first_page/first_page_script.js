cf874f4cec6f05d1e8b1761775adab450e263ef6
DCoder206 <62135445+DCoder206@users.noreply.github.com>
2023-08-09 14:39:40 +0530

    Update and rename script.js to first_page_script.js

--- website/first_page/first_page_script.js
+++ website/first_page/first_page_script.js
@@ -4,9 +4,9 @@ function redirect(loc) {
 const but = document.querySelector("#button")
 but.addEventListener("click",(event) => {
     event.preventDefault();
-    const inp = document.querySelector("#schName").value
-    if (inp.length !== 0) {
-        redirect("abc/")
+    const inp = document.querySelector("#schName").value.toLowerCase()
+    if (inp.length !== 0 && inp.includes("school")) {
+        redirect("../main_page/index.html")
     }
     else {
         const div = document.querySelector("#msg");
