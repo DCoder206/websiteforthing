0e302231bbe0d933c5061d320d73d4fd8551767d
DCoder206 <62135445+DCoder206@users.noreply.github.com>
2023-08-09 14:41:31 +0530

    Create main_page_script.js

---
+++ website/main_page/main_page_script.js
@@ -0,0 +1,18 @@
+function timer(duration,elementId) {
+    let time = duration * 60;
+    const timerele = document.querySelector("#" + elementId);
+    const interval = setInterval(() => {
+        if (time >= 0) {
+            const minutes = Math.floor(time / 60);
+            const seconds = time % 60;
+            timerele.innerHTML = `Time left<br>${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+            time--;
+        }
+        else {
+            timerele.innerHTML = "Time's up";
+            clearInterval(interval);
+        }
+    }, 1000);
+}
+const timeDiv = "timer";
+timer(90,timeDiv);
